948a646efbe8eeac1795156b8e25b1a9214cd1ab
heguilen <juan_heguilen@hotmail.com>
2022-08-23 19:00:25 -0300

    segundaEntrega

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 
 import './App.css';
-let imagen = "./1659306821532.jpg";
-let nombre = "Heguilen Juan Pablo";
-let datos = "Tengo 25 años de edad. Soy de Argentina, Bahia Blanca, provincia de Bs As. Estudiante de Tecnicatura en Optica en UNS, y de Full Stack en CoderHouse.";
+import 'core-js/actual';
+import NavBar from './components/NavBar';
+
 
 
 
 function App() {
   return (
-    <div className='container bg-dark'>
-      <div className='row '>
-        <div className='col'>
-          <img src={imagen} alt="foto de perfil" className="img-thumbnail" />
-
-        </div>
-        <div className='col '>
-          <h1 className='display-2 text-center text-white border-bottom border-3'>
-            {nombre}
-          </h1>
-          <p className='text-center fs-4 text-white'>
-            {datos}
-          </p>
-        </div>
-      </div>
+    <div>
+      <NavBar />
+      <h2>Indumentaria deportiva</h2>
     </div>
+ 
   );
 }
 
