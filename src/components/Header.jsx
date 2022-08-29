e8bf185a3421ec479c3d2b599892bdcc95059c09
heguilen <juan_heguilen@hotmail.com>
2022-08-29 20:57:38 -0300

    categorias

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,20 +9,34 @@ const Header = () => {
         <header className="container-fluid text-bg-dark ">
             <div className="row align-items-center ">
                 <div className="logo col ">
-                    <img src={logo} className="rounded-circle" alt="logoPointSport" width="140"  />
+                    <img src={logo} className="rounded-circle" alt="logoPointSport" width="140" />
                 </div>
-                
+
                 <nav className="lista col-6  text-center">
-                    <ul className="row navbar">
-                        <li className="col list-unstyled" > <a href="ASD" className="text-decoration-none text-white">Inicio</a></li>
-                        <li className="col list-unstyled"><a href="ASD" className="text-decoration-none text-white">Contacto</a></li>
-                        <li className="col list-unstyled"></li>
-                        <li className="col list-unstyled" ><a href="ASD" className="text-decoration-none text-white">Ubicacion </a></li>
-                    </ul>
+
+                    <div className="dropdown">
+                        <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                            Productos
+                        </a>
+
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <li><a className="dropdown-item" href="#">Pantalones</a></li>
+                            <li><a className="dropdown-item" href="#">Calzas</a></li>
+                            <li><a className="dropdown-item" href="#">Tops</a></li>
+                            <li><a className="dropdown-item" href="#">Conjuntos</a></li>
+                        </ul>
+                        <ul className="row navbar">
+    
+                            <li className="col list-unstyled"><a href="ASD" className="text-decoration-none text-white">Contacto</a></li>
+                            
+                            <li className="col list-unstyled" ><a href="ASD" className="text-decoration-none text-white">Ubicacion </a></li>
+                        </ul>
+                    </div>
+
                 </nav>
 
                 <div className="col  text-center">
-                   <CartWidget />
+                    <CartWidget />
                 </div>
             </div>
         </header>
